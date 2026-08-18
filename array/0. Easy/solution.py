37f953abfbb6b2a18b8f00a61550caf84bf117ec
Jairam Hegde <154826876+Jairamhegde@users.noreply.github.com>
2026-08-18 23:16:24 +0530

    0.  - Easy

--- array/0. Easy/solution.py
+++ array/0. Easy/solution.py
@@ -1,12 +1,1 @@
-            if i not in ary:
-                index += 1
-                if index == k:
-                    return i
-
-        while index < k:
-            max_ele += 1
-            index += 1
-        return max_ele
-
-
-
+c
