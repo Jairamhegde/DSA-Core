684b4de07a27cce39541990bcd8df5783dbf3d92
Jairam Hegde <154826876+Jairamhegde@users.noreply.github.com>
2026-08-21 17:09:54 +0530

    Same Tree ? Time: 0 ms (100%), Space: 12.5 MB (23.06%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        if p.val != q.val:
+            return False
+        
+        left = self.isSameTree(p.left,q.left)
+        if left :
+            return self.isSameTree(p.right,q.right)
+        else:
+            return left
+
+        
+
+        
+    
+        
+        
+
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
