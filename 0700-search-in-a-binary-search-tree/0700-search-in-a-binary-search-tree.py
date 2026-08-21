606cb4bd7b4857598f33dc425b52e844f9b3ef3d
Jairam Hegde <154826876+Jairamhegde@users.noreply.github.com>
2026-08-21 20:52:15 +0530

    Search in BST Time: 3 ms (5.17%), Space: 16.3 MB (98.55%)

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        
+        while root:
+            if val < root.val:
+             
+                root = root.left
+            elif val > root.val:
+                
+                root = root.right
+            elif val == root.val:
+                return root
+        return 
+            
+
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
