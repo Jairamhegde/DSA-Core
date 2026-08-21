aad1c17415032a881b5729545e39356a296f7fb3
Jairam Hegde <154826876+Jairamhegde@users.noreply.github.com>
2026-08-21 20:57:08 +0530

    Search BST (Recursive) Time: 0 ms (100%), Space: 16.5 MB (37.33%)

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
@@ -6,17 +6,16 @@
 #         self.right = right
 class Solution(object):
     def searchBST(self, root, val):
+        if root is None:
+            return
+        if root.val == val:
+            return root
+        if root.val > val:
+            return self.searchBST(root.left,val)
+        if root.val < val:
+            return self.searchBST(root.right,val)
         
-        while root:
-            if val < root.val:
-             
-                root = root.left
-            elif val > root.val:
-                
-                root = root.right
-            elif val == root.val:
-                return root
-        return 
+        
             
 
 
