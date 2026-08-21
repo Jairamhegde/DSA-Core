dc7298076d0d6172c033c0cb82303526842dce33
Jairam Hegde <154826876+Jairamhegde@users.noreply.github.com>
2026-08-21 19:13:22 +0530

    LCA of BST Time: 62 ms (16.27%), Space: 20.5 MB (46.53%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        if root is None :
+            return 
+        if root == p or root == q:
+            return root
+        if min(p,q) <= root:
+            left = self.lowestCommonAncestor(root.left, p, q)
+            right = self.lowestCommonAncestor(root.right, p, q)
+        else:
+            right = self.lowestCommonAncestor(root.right, p, q)
+            left = self.lowestCommonAncestor(root.left, p, q)
+        if left and right:
+            return root
+        if left:
+            return left
+        return right
+            
+            
+
+        
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
