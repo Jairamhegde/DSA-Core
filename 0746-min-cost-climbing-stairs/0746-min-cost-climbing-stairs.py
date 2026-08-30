4b0c2e84e1e8ab0f92fdb8026cda212982515bf6
Jairam Hegde <154826876+Jairamhegde@users.noreply.github.com>
2026-08-30 18:21:35 +0530

    Min Cost Climb Stairs :no extra space :Time: 4 ms (45.79%), Space: 12.4 MB (91.37%) - LeetHub - Time: O(n), Space: O(1)

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,15 +1,13 @@
 class Solution(object):
     def minCostClimbingStairs(self, cost):
         n = len(cost)
-        dp = [0] * (n+1)
-
-        min_cost = 0
-        dp[0] = 0
-        dp[1] = 0
+        prev2 = 0
+        prev1 = 0
         for i in range(2,n+1):
-            dp[i] = min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2])
-
-        return dp[-1]
+            current = min((prev1+ cost[i-1]),(prev2+cost[i-2]))
+            prev2 = prev1
+            prev1 = current
+        return prev1
         
 
 # Synced seamlessly with LeetHub Pro
