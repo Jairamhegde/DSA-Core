5e7c1c52ca339ba7a0ba06293ec77671aac97ed7
Jairam Hegde <154826876+Jairamhegde@users.noreply.github.com>
2026-08-30 15:59:08 +0530

    Climbing Stairs : Time: 0 ms (100%), Space: 12.2 MB (90.45%)  - Time: O(n), Space: O(n)

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def climbStairs(self, n):
+        if n == 1:
+            return 1
+        dp = [0]*(n+1)
+
+        dp[1] = 1
+        dp[2] = 2
+        total_ways = 0
+
+        for i in range(3,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+
+
+        
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
