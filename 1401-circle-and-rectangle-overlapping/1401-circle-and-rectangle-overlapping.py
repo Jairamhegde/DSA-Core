ba4bf0c9dfff57d8e6e6a2194466ae8173fb04f3
Jairam Hegde <154826876+Jairamhegde@users.noreply.github.com>
2026-09-19 14:10:05 +0530

    Circle-and-rectangle-overlaping : Time: 0 ms (100%), Space: 12.5 MB (15.38%) -Time: O(1), Space: O(1)

---
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def checkOverlap(self, radius, xCenter, yCenter, x1, y1, x2, y2):
+        """
+        :type radius: int
+        :type xCenter: int
+        :type yCenter: int
+        :type x1: int
+        :type y1: int
+        :type x2: int
+        :type y2: int
+        :rtype: bool
+        """
+        closestx = max(x1,min(xCenter,x2))
+        closesty = max(y1,min(yCenter,y2))
+
+        return (xCenter-closestx)**2 + (yCenter-closesty)**2 <= radius**2
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
